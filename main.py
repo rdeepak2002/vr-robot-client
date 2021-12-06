9b35786dfdd6dc720ca8005efc79a3d0cab9ab7b
rdeepak2002 <rdeepak2002@gmail.com>
2021-12-06 14:12:02 -0500

    added server auto reconnect

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import base64
 import asyncio
 import json
 import sys
+import time
 
 # get the camera
 cam = cv2.VideoCapture(0)
@@ -28,74 +29,76 @@ def update_robot(data):
 
 # main method
 async def main():
-    try:
-        async with websockets.connect(url) as websocket:
-            while True:
-                # wait for message from vr controller
-                try:
+    while True:
+        try:
+            async with websockets.connect(url) as websocket:
+                while True:
                     server_message = await websocket.recv()
 
-                    server_message_obj = json.loads(server_message)
+                    # wait for message from vr controller
+                    try:
+                        server_message_obj = json.loads(server_message)
 
-                    sender = server_message_obj['sender']
-                    data = server_message_obj['data']
+                        sender = server_message_obj['sender']
+                        data = server_message_obj['data']
 
-                    if sender == 'vr-controller':
-                        try:
-                            data_dict = json.loads(data)
-                            update_robot(data_dict)
-                        except:
-                            print("error parsing data from message", data)
-                except:
-                    print("error parsing", server_message)
+                        if sender == 'vr-controller':
+                            try:
+                                data_dict = json.loads(data)
+                                update_robot(data_dict)
+                            except:
+                                print("error parsing data from message", data)
+                    except:
+                        print("error parsing", server_message)
 
-                # get webcam frame
-                ret, frame = cam.read()
+                    # get webcam frame
+                    ret, frame = cam.read()
 
-                # scale down image so unity does not die when decoding it
-                scale_percent = 30  # percent of original size
-                width = int(frame.shape[1] * scale_percent / 100)
-                height = int(frame.shape[0] * scale_percent / 100)
-                dim = (width, height)
-                resized_frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+                    # scale down image so unity does not die when decoding it
+                    scale_percent = 30  # percent of original size
+                    width = int(frame.shape[1] * scale_percent / 100)
+                    height = int(frame.shape[0] * scale_percent / 100)
+                    dim = (width, height)
+                    resized_frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
 
-                if not ret:
-                    print("failed to grab frame")
-                    break
+                    if not ret:
+                        print("failed to grab frame")
+                        break
 
-                # uncomment to show window:
-                # cv2.imshow("camera", resized_frame)
+                    # uncomment to show window:
+                    # cv2.imshow("camera", resized_frame)
 
-                # convert frame to base 64 image
-                retval, buffer = cv2.imencode('.jpg', resized_frame)
-                base64_img_string = str(base64.b64encode(buffer.tobytes()))[2: -1]
+                    # convert frame to base 64 image
+                    retval, buffer = cv2.imencode('.jpg', resized_frame)
+                    base64_img_string = str(base64.b64encode(buffer.tobytes()))[2: -1]
 
-                # uncomment to view base64 string being printed
-                # print(base64_img_string)
+                    # uncomment to view base64 string being printed
+                    # print(base64_img_string)
 
-                # send image to socket server
-                data = {
-                    "sender": "camera",
-                    "data": base64_img_string
-                }
+                    # send image to socket server
+                    data = {
+                        "sender": "camera",
+                        "data": base64_img_string
+                    }
 
-                data_str = str(json.dumps(data))
+                    data_str = str(json.dumps(data))
 
-                # print(data_str)
+                    # print(data_str)
 
-                await websocket.send(data_str)
+                    await websocket.send(data_str)
 
-                # stop if escape key is pressed
-                k = cv2.waitKey(1)
+                    # stop if escape key is pressed
+                    # k = cv2.waitKey(1)
 
-                if k % 256 == 27:
-                    # ESC pressed
-                    print("Escape hit, closing...")
-                    break
+                    # if k % 256 == 27:
+                    #     # ESC pressed
+                    #     print("Escape hit, closing...")
+                    #     break
+        except:
+            print('reconnecting to socket server...')
+            time.sleep(3)
 
-            cam.release()
-            cv2.destroyAllWindows()
-    except:
-        print('error connecting to socket server')
+    cam.release()
+    cv2.destroyAllWindows()
 
 asyncio.get_event_loop().run_until_complete(main())
