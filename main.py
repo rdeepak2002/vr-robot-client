b550951d9a37e8f73076d4526e26d33586db8288
rdeepak2002 <rdeepak2002@gmail.com>
2021-12-07 04:01:33 -0500

    made robot data receiving faster

--- main.py
+++ main.py
@@ -63,49 +63,41 @@ async def main():
                     except:
                         print("error parsing", server_message)
 
-                    # get webcam frame
-                    ret, frame = cam.read()
-
-                    # scale down image so unity does not die when decoding it
-                    scale_percent = 30  # percent of original size
-                    width = int(frame.shape[1] * scale_percent / 100)
-                    height = int(frame.shape[0] * scale_percent / 100)
-                    dim = (width, height)
-                    resized_frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
-
-                    if not ret:
-                        print("failed to grab frame")
-                        break
-
-                    # uncomment to show window:
-                    # cv2.imshow("camera", resized_frame)
-
-                    # convert frame to base 64 image
-                    retval, buffer = cv2.imencode('.jpg', resized_frame)
-                    base64_img_string = str(base64.b64encode(buffer.tobytes()))[2: -1]
-
-                    # uncomment to view base64 string being printed
-                    # print(base64_img_string)
-
-                    # send image to socket server
-                    data = {
-                        "sender": "camera",
-                        "data": base64_img_string
-                    }
-
-                    data_str = str(json.dumps(data))
-
-                    # print(data_str)
-
-                    await websocket.send(data_str)
-
-                    # stop if escape key is pressed
-                    # k = cv2.waitKey(1)
-
-                    # if k % 256 == 27:
-                    #     # ESC pressed
-                    #     print("Escape hit, closing...")
+                    # # get webcam frame
+                    # ret, frame = cam.read()
+                    #
+                    # # scale down image so unity does not die when decoding it
+                    # scale_percent = 30  # percent of original size
+                    # width = int(frame.shape[1] * scale_percent / 100)
+                    # height = int(frame.shape[0] * scale_percent / 100)
+                    # dim = (width, height)
+                    # resized_frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+                    #
+                    # if not ret:
+                    #     print("failed to grab frame")
                     #     break
+                    #
+                    # # uncomment to show window:
+                    # # cv2.imshow("camera", resized_frame)
+                    #
+                    # # convert frame to base 64 image
+                    # retval, buffer = cv2.imencode('.jpg', resized_frame)
+                    # base64_img_string = str(base64.b64encode(buffer.tobytes()))[2: -1]
+                    #
+                    # # uncomment to view base64 string being printed
+                    # # print(base64_img_string)
+                    #
+                    # # send image to socket server
+                    # data = {
+                    #     "sender": "camera",
+                    #     "data": base64_img_string
+                    # }
+                    #
+                    # data_str = str(json.dumps(data))
+                    #
+                    # # print(data_str)
+                    #
+                    # await websocket.send(data_str)
         except:
             print('reconnecting to %s server...' % (url))
             time.sleep(3)
